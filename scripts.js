2b1da35a4357c81f39dcc184122176943ed6344f
Jahmoe Brown <jahmoenrown@gmail.com>
2023-11-05 22:37:18 -0500

    Updated Javascript code entry for mailing list

--- scripts.js
+++ scripts.js
@@ -1,18 +1,19 @@
 /* Add your JavaScript to this file */
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('emailForm');
-    const emailInput = document.getElementById('emailInput');
-    const messageDiv = document.querySelector('.message');
+window.onload = function(){
+    var emailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    var userEmail = document.getElementById("email");
+    var messageDiv = document.getElementsByClassName("message")[0];
+    var submitButton = document.getElementsByTagName("button")[0];
 
-    form.addEventListener('submit', function(event) {
-        event.preventDefault(); 
+    submitButton.addEventListener("click", function(e) {
+        e.preventDefault();
 
-        const enteredEmail = emailInput.value.trim();
-
-        if (enteredEmail === '') {
-            messageDiv.textContent = 'Please enter a valid email address.';
+        if (userEmail.value === "" || !userEmail.value.match(emailFormat)){
+            messageDiv.innerHTML = "Please enter a valid email address.";
+            userEmail.value = "";
         } else {
-            messageDiv.textContent = `Thank you! Your email address ${enteredEmail} has been added to our mailing list!`;
+            messageDiv.innerHTML = "Thank you! Your email address " + userEmail.value + " has been added to our mailing list";
         }
-    });
-});
+    })
+}
+
