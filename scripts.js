2b4b3ccde3a9a6da1decae0942f33427cc16692f
Jahmoe Brown <jahmoenrown@gmail.com>
2023-11-05 21:25:27 -0500

    Updated javascript code entry

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
 /* Add your JavaScript to this file */
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('emailForm');
+    const emailInput = document.getElementById('emailInput');
+    const messageDiv = document.querySelector('.message');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); 
+
+        const enteredEmail = emailInput.value.trim();
+
+        if (enteredEmail === '') {
+            messageDiv.textContent = 'Please enter a valid email address.';
+        } else {
+            messageDiv.textContent = `Thank you! Your email address ${enteredEmail} has been added to our mailing list!`;
+        }
+    });
+});
